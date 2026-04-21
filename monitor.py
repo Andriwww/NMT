77b37793b49c4b5e4a3cb88d72c4f33f60186aaf
Andreu Martinez <andreumartola@gmail.com>
2026-04-21 17:26:36 +0200

    formato

--- monitor.py
+++ monitor.py
@@ -4,34 +4,25 @@ from datetime import datetime
 
 def ping(host):
     param = "-n" if platform.system().lower() == "windows" else "-c"
-
     command = ["ping", param, "1", host]
-
     try:
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
-
         if "time=" in output or "tiempo=" in output:
             if "time=" in output:
                 time_part = output.split("time=")[1].split("ms")[0]
             else:
                 time_part = output.split("tiempo=")[1].split("ms")[0]
-    
             latency = time_part.strip()
         else:
             latency = None
-
         return True, latency
-
     except subprocess.CalledProcessError:
         return False, None
-
 
 def check_hosts(hosts):
     for host in hosts:
         status, latency = ping(host)
-
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
         if status:
             print(f"{timestamp} | {host} -> UP ({latency} ms)")
         else:
